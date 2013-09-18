f36b6f3fc381580354a4b42a660d4a2b8a7aefb1
Lei Wang <tendant@gmail.com>
2013-09-18 16:35:37 -0700

    add config.clj

---
+++ config.clj
@@ -0,0 +1,11 @@
+[:site-title "blog.leiwang.info"
+ :site-description "blog.leiwang.info"
+ :site-url "http://blog.leiwang.info"
+ :in-dir "resources/"
+ :out-dir "html/"
+ :default-template "default.clj"
+ :encoding "UTF-8"
+ :blog-as-index false
+ :create-archives false
+ :atomic-build true]
+
