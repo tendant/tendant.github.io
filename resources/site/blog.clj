b17c54ae13d1cee07d6b1f75407f82683dd978a2
Lei Wang <tendant@gmail.com>
2013-11-13 17:31:11 -0800

    update blog style

--- resources/site/blog.clj
+++ resources/site/blog.clj
@@ -10,7 +10,7 @@
                      "yyyy-MM-dd" "dd MMM yyyy"
                      (re-find #"\d*-\d*-\d*" (str f)))]
       [:div
-       [:h4.blog-title [:a {:href url} (:title metadata)]]
-       [:h5.blog-date date]])
+       [:h5.blog-title [:a {:href url} (:title metadata)]]
+       [:h6.blog-date date]])
       (take 8 (reverse (static.io/list-files :posts))))
    ]]]
