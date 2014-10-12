939df8234c7ac549af18e3fa157b5fc162b1c621
Lei Wang <tendant@gmail.com>
2014-10-11 22:59:29 -0700

    update blog.clj

--- resources/site/index.clj
+++ resources/site/index.clj
@@ -1,33 +1,16 @@
 {:title "blog.leiwang.info"}
 
 [:div
- ;; [:div.row
- ;;  [:div.col-md-12
- ;;   [:h1 "[untitled]"]]]
  [:div.row
-  [:div.col-md-6
-   [:p.lead.text-muted
-    "Random thoughts"]]]
- ;; [:div.row
- ;;  [:div.col-md-12
- ;;   Add self instruction.
- ;;   [:p
- ;;    ]
- ;;   [:p
- ;;    ]
- ;;   ]]
- [:div.row
-  [:div.col-md-6
-   [:h4 "Recent Blog Posts"]
+  [:div.col-md-12
+   [:h2 "Blog"]
    (map #(let [f % url (static.core/post-url f)
                [metadata _] (static.io/read-doc f)
                date (static.core/parse-date
-                     "yyyy-MM-dd" "dd MMMM yyyy"
+                     "yyyy-MM-dd" "dd MMM yyyy"
                      (re-find #"\d*-\d*-\d*" (str f)))]
       [:div
-       [:div [:a {:href url} (:title metadata)]
-       [:div date]]])
-      (take 8 (reverse (static.io/list-files :posts))))]
-    [:div.col-md-6
-    [:h4 "Recent Activity on Github"]
-    [:div.gh-recent]]]]
+       [:h5.blog-title [:a {:href url} (:title metadata)]]
+       [:h6.blog-date date]])
+      (take 8 (reverse (static.io/list-files :posts))))
+   ]]]
