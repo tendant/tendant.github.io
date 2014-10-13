caa459ac2b38d58ce4000c17b414b39537db538a
Lei Wang <tendant@gmail.com>
2014-10-12 23:38:17 -0700

    add google analytics code

--- resources/templates/default.clj
+++ resources/templates/default.clj
@@ -79,4 +79,6 @@
         {:href "http://creativecommons.org/licenses/by-sa/3.0/deed.en_US",
          :rel "license"}
         "Creative Commons Attribution-ShareAlike 3.0 Unported License"]
-       ".\n"]]]]]]]
+       ".\n"]]]]]
+  [:script
+   "\n  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');\n\n  ga('create', 'UA-6332912-4', 'auto');\n  ga('send', 'pageview');\n\n"]]]
