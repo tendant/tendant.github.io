cbcd783a6a4adf8893b99b6d4d2c151edf3d7b7f
Lei Wang <tendant@gmail.com>
2015-04-19 13:11:38 -0700

    update language config

--- config.clj
+++ config.clj
@@ -13,5 +13,17 @@
                       (org-html-export-as-html nil nil nil t nil)
                       (switch-to-buffer "*Org HTML Export*")
                       (princ (buffer-string)))
+ :emacs-eval ['(set-language-environment 'Chinese-GB)
+              '(setq locale-coding-system 'utf-8)
+              '(set-terminal-coding-system 'utf-8)
+              '(set-keyboard-coding-system 'utf-8)
+              '(set-selection-coding-system 'utf-8)
+              '(prefer-coding-system 'utf-8)
+              '(require 'un-define "un-define" t)
+              '(set-buffer-file-coding-system 'utf-8 'utf-8-unix)
+              '(set-default buffer-file-coding-system 'utf-8-unix)
+              '(set-default-coding-systems 'utf-8-unix)
+              '(prefer-coding-system 'utf-8-unix)
+              '(set-default default-buffer-file-coding-system 'utf-8-unix)]
  :atomic-build true]
 
